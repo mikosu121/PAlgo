f16e3e3b2d5c678adb79db54b1b195950acf379e
mikosu121 <farrelrassya23@gmail.com>
2025-05-16 13:43:07 +0700

    Add files via upload

---
+++ index.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+struct mahasiswa {
+    string nama;
+    int NIM;
+    int nilai;
+    mahasiswa* next;
+    mahasiswa* prev;
+};
+
+int main()
+{
+    
+    
+
+    return 0;
+}
